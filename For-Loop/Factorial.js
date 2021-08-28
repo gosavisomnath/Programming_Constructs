24f5c2a5e6c4b3ca688cbf09160460253cadaa6f
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-08-28 13:10:57 +0530

    Java Script Using For Loop

---
+++ For-Loop/Factorial.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+const number = Number(prompt("Enter a Number : "));
+
+let factorial = 1;
+
+for (let index = 1; index <= number; index++) {
+    factorial = factorial * index ;
+    
+}
+console.log("Factorial Of " +number+ " is " +factorial);
---
+++ For-Loop/HarmonicNumber.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+const number = Number(prompt("Enter a Number :"));
+
+let harmonicNumber = 1;
+
+for (let index = 2; index <= number; index++) {
+    harmonicNumber += 1 / index;
+}
+
+console.log("Harmonic Number Is :" +harmonicNumber);
